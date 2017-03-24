339fb2caac62761264893e8f42efed1355a31aa4
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-03-24 22:40:35 +0000

    Services done friday

---
+++ database.sql
@@ -0,0 +1,17 @@
+# Sequence for client id 
+
+CREATE sequence seq_clientId start with 1 increment by 1; 
+
+# Stores clients information 
+CREATE TABLE client ( 
+id NUMBER(10), 
+email VARCHAR(255), 
+password VARCHAR(255), 
+district VARCHAR(255), 
+county VARCHAR(255), 
+street VARCHAR(255), 
+door_number VARCHAR(255), 
+zip_code VARCHAR(255), 
+phone_number VARCHAR(20), 
+PRIMARY KEY(email) 
+);
