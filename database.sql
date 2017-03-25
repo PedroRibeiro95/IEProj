fb789fad9d3a00b09bf0b8a56d76d47172133b45
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-03-25 23:44:34 +0000

    First service only needs to send SMS and Email

--- database.sql
+++ database.sql
@@ -7,11 +7,7 @@ CREATE TABLE client (
 id NUMBER(10), 
 email VARCHAR(255), 
 password VARCHAR(255), 
-district VARCHAR(255), 
-county VARCHAR(255), 
-street VARCHAR(255), 
-door_number VARCHAR(255), 
-zip_code VARCHAR(255), 
+address VARCHAR(255), 
 phone_number VARCHAR(20), 
 PRIMARY KEY(email) 
 );
