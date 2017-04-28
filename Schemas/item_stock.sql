729c714a615ab0c6eb5d91e358a4b8d182c3dd01
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-04-28 22:00:05 +0100

    New schema and some of the BPMN process

---
+++ Schemas/item_stock.sql
@@ -0,0 +1,5 @@
+# Stores item stock
+CREATE TABLE stock ( 
+itemid NUMBER(10),
+PRIMARY KEY(itemid) 
+);
--- Schemas/shopping_cart.sql
+++ Schemas/shopping_cart.sql
@@ -3,7 +3,7 @@
 CREATE sequence cart_id start with 1 increment by 1; 
 
 # Stores clients information 
-CREATE TABLE client ( 
+CREATE TABLE cart ( 
 id NUMBER(10),
 userid NUMBER(10),
 itemid NUMBER(10),
