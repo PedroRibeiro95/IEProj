5dd4dfebca6775e791295a2b7c36e7630bebb674
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-04-29 17:13:21 +0100

    Changed cart db

--- Schemas/shopping_cart.sql
+++ Schemas/shopping_cart.sql
@@ -3,10 +3,9 @@
 CREATE sequence cart_id start with 1 increment by 1; 
 
 # Stores clients information 
-CREATE TABLE cart ( 
-id NUMBER(10),
+CREATE TABLE cart (
 userid NUMBER(10),
 itemid NUMBER(10),
 quantity NUMBER(10), 
-PRIMARY KEY(id) 
+PRIMARY KEY(userid, itemid)
 );
